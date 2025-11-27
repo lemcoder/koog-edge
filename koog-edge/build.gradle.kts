f0db347dd3ebac1900c3cee81f537a6fb8b03e6f
mikolaj <lemanski.dev@gmail.com>
2025-11-27 21:29:46 +0100

    Configure publishing

--- koog-edge/build.gradle.kts
+++ koog-edge/build.gradle.kts
@@ -1,7 +1,10 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.multiplatform)
-    id("maven-publish")
+    alias(libs.plugins.maven.publish)
+    signing
 }
 
 group = "io.github.lemcoder"
@@ -59,9 +62,60 @@ android {
 publishing {
     repositories {
         mavenLocal()
-        maven {
-            name = "localRepo"
-            url = rootDir.toURI()
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    coordinates(
+        groupId = group.toString(),
+        artifactId = "koog-edge",
+        version = version.toString()
+    )
+
+    println(SonatypeHost.DEFAULT.toString())
+
+    pom {
+        name.set("Koog Edge")
+        description.set("Kotlin Multiplatform library for integrating Koog with local on device SLMs.")
+        inceptionYear.set("2025")
+        url.set("https://github.com/lemcoder/koog-edge")
+
+        licenses {
+            license {
+                name.set("Apache-2.0 license")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("lemcoder")
+                name.set("Mikołaj Lemański")
+                email.set("lemanski.dev@gmail.com")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/lemcoder/koog-edge")
+        }
+    }
+}
+
+// Sign with default plugin
+if (System.getenv("SIGNING_KEY") != null && System.getenv("SIGNING_KEY_PASSWORD") != null) {
+    signing {
+        useInMemoryPgpKeys(
+            System.getenv("SIGNING_KEY"),
+            System.getenv("SIGNING_KEY_PASSWORD")
+        )
+        sign(publishing.publications)
+
+        // Temporary workaround, see https://github.com/gradle/gradle/issues/26091#issuecomment-1722947958
+        tasks.withType<AbstractPublishToMaven>().configureEach {
+            val signingTasks = tasks.withType<Sign>()
+            mustRunAfter(signingTasks)
         }
     }
 }
